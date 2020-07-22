5ef0d8d57c13efe022d77eb6cf2d466bb735c89c
Michał Maciejewski <maciejewskimichal7@gmail.com>
2020-07-22 13:08:58 +0200

    featues: add details page with data and flag
    - add details info interfaces
    - add data to detail components and display it
    - fix details.slice state and add reducers

--- src/components/Detail/Detail.tsx
+++ src/components/Detail/Detail.tsx
@@ -1,32 +1,80 @@
 import * as React from 'react';
-import { useParams } from 'react-router-dom'
+import {useEffect} from 'react';
+import {useParams} from 'react-router-dom'
 import {useDispatch, useSelector} from "react-redux";
-import {fetchCountriesByCodeThunk, selectDetails} from "../../store/details.slice";
-import {useEffect} from "react";
+import {DetailsInfo} from '../../store/details.interfaces'
+import {
+    fetchCountriesByCodeThunk,
+    selectCurrentDetailsCountryAlphaCode,
+    selectDetails
+} from "../../store/details.slice";
+import styles from './Detail.module.scss'
 
 interface Props {
 }
 
-const Detail: React.FC<Props> = (props) => {
+const Detail: React.FC<Props> = () => {
 
-    let details = useSelector(selectDetails);
+    const details: DetailsInfo = useSelector(selectDetails);
+    const countryCode = useSelector(selectCurrentDetailsCountryAlphaCode);
     const dispatch = useDispatch();
-    const { id } = useParams();
+    const {id} = useParams();
 
     useEffect(() => {
-        dispatch(fetchCountriesByCodeThunk(id));
 
-        return () => {
-            details = {}
+        if (countryCode !== id) {
+            dispatch(fetchCountriesByCodeThunk(id));
         }
-    },[]);
 
-    console.log('d', details);
+    }, []);
+    const {flag, name, nativeName, population, region, subregion, capital, topLevelDomain, currencies, languages, borders} = details;
+
+    console.log(borders);
 
     return (
-        <div>
-            Detail works ! {id}
+        <div className={styles['detail__wrapper']}>
+            <section className={styles['detail__flag']} style={{backgroundImage: `url(${flag})`}}/>
+            <section className={styles['detail__infoWrapper']}>
+                <h1> {name} </h1>
+                <div className={styles['detail__infoDetails']}>
+                    <div>
+                        <p>Native name: <strong>{nativeName}</strong></p>
+                        <p>population: <strong>{population}</strong></p>
 
+                        <p>Region: <strong>{region}</strong></p>
+                        <p>Subregion: <strong>{subregion}</strong></p>
+                        <p>Capital: <strong>{capital}</strong></p>
+                    </div>
+                    <div>
+                        {topLevelDomain &&
+                        <p>Top level domain: {
+                            topLevelDomain.map(domain => <strong key={domain}>{domain}</strong>)
+                        }</p>
+                        }
+                        {currencies &&
+                        <p>Currencies: {
+                            currencies.map(currency => <strong key={currency.code}>{currency.name}</strong>)
+                        } </p>
+                        }
+                        {languages &&
+                        <p>Languages:
+                            <strong>
+                                {languages.map(lang => <span key={lang.iso639_2}> {lang.name}</span>)}
+                            </strong>
+                        </p>
+                        }
+
+                    </div>
+                </div>
+                <div>
+                    {borders &&
+                    <p>Border countries:
+                        {borders.map(country => <strong className={styles['detail__borderButton']}
+                                                        key={country}> {country}</strong>)}
+                    </p>
+                    }
+                </div>
+            </section>
 
         </div>
     );
--- src/store/details.slice.ts
+++ src/store/details.slice.ts
@@ -1,10 +1,16 @@
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {RootState} from './store';
-import { fetchCountryByAlpha2Code } from "../servies/countries.service";
+import {fetchCountryByAlpha2Code} from "../servies/countries.service";
+import {DetailsInfo} from "./details.interfaces";
 
-interface IInitialState {}
+interface InitialState {
+    data: DetailsInfo
+}
 
-const initialState: IInitialState = {};
+const initialState: InitialState = {
+    data: {}
+};
+
 
 export const fetchCountriesByCodeThunk = createAsyncThunk(
     "details/fetchCountriesData",
@@ -18,17 +24,23 @@ const details = createSlice({
     name: "details",
     initialState,
     reducers: {
-        
+        clearDetailsInfo: (state): void => {
+            state.data = {}
+        }
     },
     extraReducers: {
 
         [fetchCountriesByCodeThunk.fulfilled as any]: (state, action) => {
-            // @ts-ignore
-            state.details = action.payload;
+            state.data = action.payload;
         },
     }
 });
-// @ts-ignore
-export const selectDetails = (state: RootState) => state.details;
+
+export const selectDetails = (state: RootState) => state.details.data;
+export const selectCurrentDetailsCountryAlphaCode = (state: RootState) => {
+    return state.details.data.alpha3Code;
+};
+
+export const {clearDetailsInfo} = details.actions;
 
 export default details.reducer;
